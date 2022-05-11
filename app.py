1961ab2b956dc01d9d65df2bf53469981bffa31c
BergeDios <santiago.goyret@gmail.com>
2022-05-11 13:15:20 -0300

    things

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 from data import Events
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.events_db
+eventos = db.eventos
+
 Events = Events()
 
 @app.route('/')
